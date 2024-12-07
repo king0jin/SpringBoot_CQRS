ffdfdc8e3990577d3b7cbfa438f03f60b8d52cca
king0jin <et0709@naver.com>
2024-12-07 15:23:20 +0900

    CORS 설정

---
+++ src/main/java/naver/et0709/spring_cqrs/WebConfig.java
@@ -0,0 +1,15 @@
+package naver.et0709.spring_cqrs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//CORS설정을 하기위한 WebConfig클래스
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
+    }
+
+}
