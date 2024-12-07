1d7980f3db16f4f00a52199dc527c33d2c83c690
king0jin <et0709@naver.com>
2024-12-07 15:16:22 +0900

    write project

---
+++ src/main/java/naver/et0709/spring_cqrs/SpringCqrsApplication.java
@@ -0,0 +1,13 @@
+package naver.et0709.spring_cqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCqrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCqrsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/naver/et0709/spring_cqrs/SpringCqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package naver.et0709.spring_cqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCqrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
